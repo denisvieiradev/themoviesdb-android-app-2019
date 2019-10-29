cd5e78bfc426a51f79d3ffaa09a3344f838c3468
Denis Vieira <denisvieira05@gmail.com>
2019-10-29 06:35:32 -0300

    develop Fix release date nullable condition
    -
    
    Signed-off-by: Denis Vieira <denisvieira05@gmail.com>

--- app/src/main/java/org/js/denisvieira/themoviedbapp/domain/model/movie/Movie.kt
+++ app/src/main/java/org/js/denisvieira/themoviedbapp/domain/model/movie/Movie.kt
@@ -17,7 +17,7 @@ data class Movie(
     @Json(name = "genre_ids") val genreIds: List<Int>?,
     @Json(name = "poster_path") val posterPath: String?,
     @Json(name = "backdrop_path") val backdropPath: String?,
-    @Json(name = "release_date") private val releaseDate: String
+    @Json(name = "release_date") private val releaseDate: String?
 ) {
     fun getReleaseDate() : Date? {
         val formatter = SimpleDateFormat("yyyy-MM-dd")
